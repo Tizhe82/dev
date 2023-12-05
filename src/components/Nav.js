25380af8fb747f0e0c49507d9fb6c5f728b98abb
Tizhe82 <tizhepaul@gmail.com>
2023-12-05 00:12:09 -0800

    new portfolio website

---
+++ src/components/Nav.js
@@ -0,0 +1,111 @@
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
+
+export default function Nav() {
+  return (
+    <>
+      <div id="nav-bar">
+        <a href="https://tizhe82.github.io/Portfolio/">
+          <img
+            className="logo"
+            src="https://api.dicebear.com/7.x/lorelei/svg?flip=false"
+            alt="logo"
+          />
+        </a>
+
+        <nav>
+          <NavLink
+            to={process.env.PUBLIC_URL + "/"}
+            className="link"
+            activeClassName="active-nav"
+            exact
+          >
+            <i className="fas fa-home fa-2x"></i>
+            <div className="overlay">
+              <div className="text">HOME</div>
+            </div>
+          </NavLink>
+          <NavLink
+            to={process.env.PUBLIC_URL + "/about"}
+            className="link"
+            activeClassName="active-nav"
+          >
+            <i className="image fas fa-user fa-2x"></i>
+            <div className="overlay">
+              <div className="text">ABOUT</div>
+            </div>
+          </NavLink>
+          <NavLink
+            to={process.env.PUBLIC_URL + "/skills"}
+            className="link"
+            activeClassName="active-nav"
+          >
+            <i className="fas fa-cogs fa-2x"></i>
+            <div className="overlay">
+              <div className="text">SKILLS</div>
+            </div>
+          </NavLink>
+          <NavLink
+            to={process.env.PUBLIC_URL + "/work"}
+            className="link"
+            activeClassName="active-nav"
+          >
+            <i className="fas fa-eye fa-2x"></i>
+            <div className="overlay">
+              <div className="text">MY WORK</div>
+            </div>
+          </NavLink>
+          <NavLink
+            to={process.env.PUBLIC_URL + "/contact"}
+            className="link"
+            activeClassName="active-nav"
+          >
+            <i className="fas fa-envelope fa-2x"></i>
+            <div className="overlay">
+              <div className="text">CONTACT</div>
+            </div>
+          </NavLink>
+        </nav>
+        <ul>
+          <li>
+            <a
+              href="https://www.instagram.com/boy_tizzle/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-instagram"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://www.reddit.com/user/Tizhe82"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-reddit-alien"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-itch-io"></i>
+            </a>
+          </li>
+        </ul>
+      </div>
+
+      <Link className="bkmrk" target="_blank"
+        to="/resume"
+      >
+        <img
+          src="https://raw.githubusercontent.com/smrnjeet222/smrnjeet222/master/assets/blogdark.png"
+          alt="Resume"
+        />
+        <span className="folded-corner"></span>
+      </Link>
+    </>
+  );
+}
---
+++ src/components/Skills.js
@@ -0,0 +1,72 @@
+import React, { useEffect } from "react";
+import bounce from "../script/bounce";
+
+export default function Skills() {
+  useEffect(() => {
+    document.title = "Simranjeet Singh | Skills ";
+
+    bounce();
+  }, []);
+
+  return (
+    <>
+      <div className="container home-page">
+        <span className="tags top-tags"> &nbsp;&nbsp;&nbsp;&lt;body&gt;</span>
+        <div className="text-zone">
+          <h1
+            aria-label=" Hi, I’m Simranjeet,web developer."
+            className="blast-root"
+          >
+            <span className="blast" style={{ color: "#23ffde" }}>
+              Skills&nbsp;
+            </span>
+            <span className="blast" style={{ color: "#23ffde" }}>
+              &&nbsp;
+            </span>
+            <span className="blast" style={{ color: "#23ffde" }}>
+              Experience
+            </span>
+          </h1>
+          <p align="LEFT">
+            The main area of my expertise is Frontend Development  and Web development.
+            <br />
+            <br />
+            Tech I know:
+            &nbsp;&nbsp; <b>Javascript &nbsp;|&nbsp; Ruby &nbsp;|&nbsp; TypeScript   &nbsp;|&nbsp;  Rust </b><br />
+            <br />
+            &nbsp;&nbsp; • &nbsp;&nbsp; ReactJs, NextJs , GatsbyJs , Svelte<br />
+            &nbsp;&nbsp; • &nbsp;&nbsp; NodeJs , Express , MongoDB<br />
+            &nbsp;&nbsp; • &nbsp;&nbsp; SQL , AWS, GCP, and Netlify<br />
+            &nbsp;&nbsp; • &nbsp;&nbsp; JavaScript + Ruby + Rust New Bee<br />
+            <br />
+            <br />
+            Visit my&nbsp;
+            <a
+              href="https://www.linkedin.com/in/tizhepaul"
+              style={{ color: "#23ffde" }}
+            >
+              LinkedIn&nbsp;
+            </a>
+            profile for more details or just&nbsp;
+            <a href="/contact" style={{ color: "#23ffde" }}>
+              contact&nbsp;
+            </a>
+            me.
+          </p>
+        </div>
+
+        <span className="tags bottom-tags" style={{ bottom: "10px" }}>
+          &nbsp;&nbsp;&nbsp;&lt;/body&gt;
+          <br />
+          &lt;/html&gt;
+        </span>
+      </div>
+      <div id="mysvg">
+        <img
+          alt="hacker"
+          src={`https://raw.githubusercontent.com/smrnjeet222/smrnjeet222/master/assets/code${Math.floor(Math.random() * 3) + 2}.svg`}
+        />
+      </div>
+    </>
+  );
+}
